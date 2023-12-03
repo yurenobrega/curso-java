5c0328c4eacdc0ace0757ce263d6ecb11199978a
Yure Nobrega <yurenobrega221b@gmail.com>
2023-12-03 12:21:52 -0300

    Arquivos da aula de comando de atribuição e casting
    Processamento de dados em Java

---
+++ estrutura_sequencial/processamento_de_dados/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        /* double b, B, h, area;
+
+        b = 6.0;
+        B = 8.0;
+        h = 5.0;
+
+        area = (b + B) / 2.0 * h;
+
+        System.out.println(area); */
+
+        /* int a, b;
+        double resultado;
+
+        a = 5;
+        b = 2;
+
+        resultado = (double) a / b;
+
+        System.out.println(resultado); */
+
+        double a;
+        int b;
+
+        a = 5.0;
+        b = (int) a;
+
+        System.out.println(b);
+    }
+}
