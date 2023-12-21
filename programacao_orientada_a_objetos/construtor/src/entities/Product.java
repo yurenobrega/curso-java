f72c4ac73c6bc6051ef74a5e41824a98b34103f3
Yure Nobrega <yurenobrega221b@gmail.com>
2023-12-20 22:43:42 -0300

    Arquivos das aulas sobre construtor, vetores e exercicios

--- programacao_orientada_a_objetos/construtor/src/entities/Product.java
+++ programacao_orientada_a_objetos/construtor/src/entities/Product.java
@@ -5,38 +5,29 @@ public class Product {
     private double price;
     private int quantity;
 
-    public Product() {
-
-    }
-
     public Product(String name, double price, int quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
     }
 
-    public Product(String name, double price) {
-        this.name = name;
-        this.price = price;
+    public String getName() {
+        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setPrice(double price){
-        this.price = price;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public double getPrice(){
+    public double getPrice() {
         return price;
     }
 
-    public int getQuantity(){
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
         return quantity;
     }
 
