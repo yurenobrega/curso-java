99669007a8f2c45f798d65a4455f621b0371765c
Yure Nobrega <yurenobrega221b@gmail.com>
2023-12-28 22:10:55 -0300

    Arquivo da aula sobre enumerações

---
+++ enumeracoes_composicao/enumeracoes/src/entities/Order.java
@@ -0,0 +1,51 @@
+package entities;
+
+import entities.enums.OrderStatus;
+
+import java.time.LocalDateTime;
+
+public class Order {
+
+    private Integer id;
+    private LocalDateTime moment;
+    private OrderStatus status;
+
+    public Order(Integer id, OrderStatus status){
+        this.id = id;
+        this.moment = LocalDateTime.now();
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getMoment() {
+        return moment;
+    }
+
+    public void setMoment(LocalDateTime moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", moment=" + moment +
+                ", status=" + status +
+                '}';
+    }
+}
