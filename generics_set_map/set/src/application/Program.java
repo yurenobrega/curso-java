6eb572e8c2a694d7b868733d3b91134c1d577428
Yure Nobrega <yurenobrega221b@gmail.com>
2024-01-25 19:12:06 -0300

    Aula sobre Set

---
+++ generics_set_map/set/src/application/Program.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Program {
+    public static void main(String[] args) {
+        Set<String> set = new LinkedHashSet<>();
+
+        set.add("TV");
+        set.add("Tablet");
+        set.add("Notebook");
+
+        set.removeIf(x -> x.charAt(0) == 'T');
+
+        for(String p: set){
+            System.out.println(p);
+        }
+    }
+
+}
