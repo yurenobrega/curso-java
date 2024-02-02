0530d7e467f5235d5e1ac6a10a54af985e2226aa
yure <yurenobrega221b@gmail.com>
2024-02-01 22:57:51 -0300

    Aulas sobre predicate, consumer e function

---
+++ programacao_funcional_lambda/consumer/src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+import entities.Product;
+import util.PriceUpdate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.function.Consumer;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.00));
+        list.add(new Product("HD Case", 80.90));
+
+        list.forEach(p -> p.setPrice(p.getPrice() * 1.1));
+
+        list.forEach(System.out::println);
+    }
+}
