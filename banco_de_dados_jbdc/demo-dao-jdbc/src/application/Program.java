662a71a18662b9bbaa7dbb508b61fefdc97bf4d7
yure <yurenobrega221b@gmail.com>
2024-02-20 19:08:44 -0300

    Implementação de findById

--- banco_de_dados_jbdc/demo-dao-jdbc/src/application/Program.java
+++ banco_de_dados_jbdc/demo-dao-jdbc/src/application/Program.java
@@ -2,7 +2,6 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,11 +9,10 @@ import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
-
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
 
--- banco_de_dados_jbdc/demo-dao-jdbc/src/model/dao/DaoFactory.java
+++ banco_de_dados_jbdc/demo-dao-jdbc/src/model/dao/DaoFactory.java
@@ -1,10 +1,10 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
-import model.entities.Seller;
 
 public class DaoFactory {
-    public static SellerDao createSellerDao(){
-        return new SellerDaoJDBC();
-    };
+    public static SellerDao createSellerDao() {
+        return new SellerDaoJDBC(DB.getConnection());
+    }
 }
