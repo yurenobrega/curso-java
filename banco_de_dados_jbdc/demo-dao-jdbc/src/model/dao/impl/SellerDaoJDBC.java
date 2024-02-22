0fe83583ff3429c513925c4737e7e39a78be3d4f
yure <yurenobrega221b@gmail.com>
2024-02-22 13:14:05 -0300

    Implementação insert

--- banco_de_dados_jbdc/demo-dao-jdbc/src/model/dao/impl/SellerDaoJDBC.java
+++ banco_de_dados_jbdc/demo-dao-jdbc/src/model/dao/impl/SellerDaoJDBC.java
@@ -6,10 +6,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -24,6 +21,39 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void insert(Seller obj) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement(
+                    "INSERT INTO seller "
+                            + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+                            + "VALUES "
+                            + "(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+            st.setDouble(4, obj.getBaseSalary());
+            st.setInt(5, obj.getDepartment().getId());
+
+            int rowsAffected = st.executeUpdate();
+
+            if(rowsAffected > 0){
+                ResultSet rs = st.getGeneratedKeys();
+                if(rs.next()){
+                    int id = rs.getInt(1);
+                    obj.setId(id);
+                }
+                DB.closeResultSet(rs);
+            } else{
+                throw new DbException("Unexpected error! No rows affected!");
+            }
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally{
+            DB.closeStatement(st);
+        }
+
 
     }
 
@@ -99,7 +129,7 @@ public class SellerDaoJDBC implements SellerDao {
             while (rs.next()) {
                 Department dep = map.get(rs.getInt("DepartmentId"));
 
-                if( dep == null){
+                if (dep == null) {
                     dep = instantieteDepartment(rs);
                     map.put(rs.getInt("DepartmentId"), dep);
                 }
@@ -138,7 +168,7 @@ public class SellerDaoJDBC implements SellerDao {
             while (rs.next()) {
                 Department dep = map.get(rs.getInt("DepartmentId"));
 
-                if( dep == null){
+                if (dep == null) {
                     dep = instantieteDepartment(rs);
                     map.put(rs.getInt("DepartmentId"), dep);
                 }
